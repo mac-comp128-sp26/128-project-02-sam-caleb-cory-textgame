f5e32763883fb7772a8b06d051fb7784134d33aa
captaincookie886 <45984741+Tehcnalties@users.noreply.github.com>
2026-04-20 13:34:51 -0500

    added file reader

--- src/util/ReadLn.java
+++ src/util/ReadLn.java
@@ -1,5 +1,23 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReadLn {
+
+    private BufferedReader reader;
+
+    public ReadLn(String filename) throws IOException {
+        reader = new BufferedReader(new FileReader(filename));
+    }
+
+    public String readLine() throws IOException {
+        return reader.readLine();
+    }
+
+    public void close() throws IOException {
+        reader.close();
+    }
     
 }
